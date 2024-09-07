69a0829a416ba9c70bd2930c2033bf4658f70be8
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-07 12:10:36 -0300

    Aprendendo sobre tipos primitivos em Java

---
+++ inicio/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+  public static void main(String[] args) {
+    // // Tipos numéricos inteiros
+    // byte anosDeEmpresa = 23;
+    // short numeroDeVoos = 43;
+    // int id = 56789;
+    // long pontosAcumulados = 3_184_321_321L;
+
+    // // Tipos numéricos reais
+    // float salario = 11_445.44F;
+    // double vendasAcumuladas = 2_991_797_103.01;
+
+    // Tipo booleadno
+    // boolean estaDeFerias = false; // true
+
+
+    // Tipo caractere
+    // char status = 'a';
+  }
+}

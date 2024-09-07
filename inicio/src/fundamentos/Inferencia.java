4e50ec03859d462fbffaaa0aaa20467746e9bb2e
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-07 12:11:30 -0300

    Aprendendo sobre inferencia em java

---
+++ inicio/src/fundamentos/Inferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Inferencia {
+  public static void main(String[] args) {
+    // var b = 4.5;
+    // Java possui inferência de tipo
+
+    // As variáveis em java são sempre do mesmo tipo
+    // b = "Texto";
+    // Isso não pode devido ao fato de estarmos mudando o tipo da variável
+  }
+}

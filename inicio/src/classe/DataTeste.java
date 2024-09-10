5af8f60ec21ffd4fe970aaadf580225a232a1296
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-10 16:31:32 -0300

    inicio sobre classe e métodos

---
+++ inicio/src/classe/DataTeste.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class DataTeste {
+  public static void main(String[] args) {
+    Data d1 = new Data();
+
+    d1.dia = 9;
+    d1.mes = 10;
+    d1.ano = 2002;
+
+    d1.imprimirDataFormatada();
+
+  }
+}

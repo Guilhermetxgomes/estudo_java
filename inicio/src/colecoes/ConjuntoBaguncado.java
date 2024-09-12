35f761fca0c20d0588de283d25c9f3283741181b
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-12 18:30:19 -0300

    Aprendendo sobre fila

--- inicio/src/colecoes/ConjuntoBaguncado.java
+++ inicio/src/colecoes/ConjuntoBaguncado.java
@@ -1,39 +1,39 @@
-package colecoes;
+// package colecoes;
 
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
 
-public class ConjuntoBaguncado {
-  public static void main(String[] args) {
-    HashSet conjunto = new HashSet();
+// public class ConjuntoBaguncado {
+//   public static void main(String[] args) {
+//     HashSet conjunto = new HashSet();
 
-    conjunto.add(1.2); // double -> Double
-    conjunto.add(true); // boolean -> Boolean
-    conjunto.add("teste"); // String
-    conjunto.add(2); // int -> Integer
-
-
-    System.out.println(conjunto.size());
+//     conjunto.add(1.2); // double -> Double
+//     conjunto.add(true); // boolean -> Boolean
+//     conjunto.add("teste"); // String
+//     conjunto.add(2); // int -> Integer
 
 
-    System.out.println(conjunto.remove("teste"));
-    System.out.println(conjunto.remove("Teste"));
-
-    System.out.println(conjunto.contains(2));
-
-    Set nums = new HashSet();
-    nums.add(1);
-    nums.add(2);
-    nums.add(3);
-
-    System.out.println(nums);
-
-    // conjunto.addAll(nums);
-
-    conjunto.retainAll(nums);
-
-    System.out.println(conjunto);
+//     System.out.println(conjunto.size());
 
 
-  }
-}
+//     System.out.println(conjunto.remove("teste"));
+//     System.out.println(conjunto.remove("Teste"));
+
+//     System.out.println(conjunto.contains(2));
+
+//     Set nums = new HashSet();
+//     nums.add(1);
+//     nums.add(2);
+//     nums.add(3);
+
+//     System.out.println(nums);
+
+//     // conjunto.addAll(nums);
+
+//     conjunto.retainAll(nums);
+
+//     System.out.println(conjunto);
+
+
+//   }
+// }

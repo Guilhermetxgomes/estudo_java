04fbc6ecfd7ec8ee3e71135a97a6033cf5aba937
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-18 16:37:09 -0300

    Criando método de abertura de campos

--- campo_minado/campo_minado/src/modelo/Campo.java
+++ campo_minado/campo_minado/src/modelo/Campo.java
@@ -3,6 +3,8 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import excecao.ExplosaoException;
+
 public class Campo {
 	
 	private final int linha;
@@ -37,6 +39,36 @@ public class Campo {
 		}
 		
 		return false;
+		
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		}
+		
+		
+		return false;
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
 		
 	}
 	

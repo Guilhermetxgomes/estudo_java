cc7b8de3fd319fef052c91560e3c4b597284eee5
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-19 11:00:23 -0300

    Criando o tabuleiro

--- campo_minado/campo_minado/src/modelo/Tabuleiro.java
+++ campo_minado/campo_minado/src/modelo/Tabuleiro.java
@@ -1,5 +1,40 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+	}
+
+
+	private void gerarCampos() {
+		for (int i = 0; i )
+		
+	}
+	
+	private void associarOsVizinhos() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void sortearMinas() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }

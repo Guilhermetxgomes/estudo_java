e001afcbbd4af7cea15089ca1635a7728cf7b06c
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-28 15:49:04 -0300

    Aprendendo sobre PathVariable

--- demo/src/main/java/com/example/demo/controler/ClienteControler.java
+++ demo/src/main/java/com/example/demo/controler/ClienteControler.java
@@ -1,17 +1,25 @@
 package com.example.demo.controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Cliente;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteControler {
 
 
-  @GetMapping(path = "/clientes/qualquer")
+  @GetMapping(path = "/qualquer")
   public Cliente obterCliente() {
 
     return new Cliente(21,"Gui", "12341234");
+  }
+
+  @GetMapping("/{id}")
+  public Cliente obterClientePorId1(@PathVariable int id) {
+    return new Cliente(id, "Maria", "131232131");
   }
 }
--- demo/src/main/java/com/example/demo/controler/metodoHTTPControler.java
+++ demo/src/main/java/com/example/demo/controler/metodoHTTPControler.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class metodoHTTPControler {
 
   @PostMapping
   public String post() {
-    return "Requisição GET";
+    return "Requisição post";
   }
 
   @PutMapping

23bbc297e828f8c8dccdb53358a2aab00368cf1b
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-28 15:59:30 -0300

    Aprendedo sobre RequestParam

--- demo/src/main/java/com/example/demo/controler/ClienteControler.java
+++ demo/src/main/java/com/example/demo/controler/ClienteControler.java
@@ -3,6 +3,7 @@ package com.example.demo.controler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Cliente;
@@ -21,5 +22,11 @@ public class ClienteControler {
   @GetMapping("/{id}")
   public Cliente obterClientePorId1(@PathVariable int id) {
     return new Cliente(id, "Maria", "131232131");
+  }
+
+
+  @GetMapping
+  public Cliente obterClientePorId2(@RequestParam(name = "id") int id) {
+    return new Cliente(id, "Joao ", "321231321");
   }
 }

debc84635f07080fbce5f8a038526c629f13c538
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-29 09:24:30 -0300

    Consulta paginada

--- demo/src/main/java/com/example/demo/controller/ProdutoController.java
+++ demo/src/main/java/com/example/demo/controller/ProdutoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +38,12 @@ public class ProdutoController {
     return produtoRepository.findAll();
   }
 
+  @GetMapping("/pagina/{numeroPagina}")
+  public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina) {
+    Pageable page = PageRequest.of(numeroPagina,1);
+    return produtoRepository.findAll(page);
+  }
+
   @GetMapping("/{id}")
   public Optional<Produto> obterProdutoPorId(@PathVariable int id){
     return produtoRepository.findById(id);
@@ -52,4 +60,5 @@ public class ProdutoController {
   public void excluirProduto(@PathVariable int id) {
     produtoRepository.deleteById(id);
   }
+
 }
--- demo/src/main/java/com/example/demo/model/repositories/ProdutoRepository.java
+++ demo/src/main/java/com/example/demo/model/repositories/ProdutoRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.model.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example.demo.model.entities.Produto;
 
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>, CrudRepository<Produto,Integer> {
 
 
 

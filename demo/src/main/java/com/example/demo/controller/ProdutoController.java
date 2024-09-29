ebb7019ebbc53a24c245c670f079321418e0b3c1
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-29 09:00:13 -0300

    Deletando dados

--- demo/src/main/java/com/example/demo/controller/ProdutoController.java
+++ demo/src/main/java/com/example/demo/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +48,8 @@ public class ProdutoController {
   //   return produto;
   // }
 
+  @DeleteMapping("/{id}")
+  public void excluirProduto(@PathVariable int id) {
+    produtoRepository.deleteById(id);
+  }
 }
